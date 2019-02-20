743f00dac110a2d0ee7cb2aa3629f9e9e5c2ea73
liao <284983325@qq.com>
2019-02-20 15:51:07 +0800

    Modify data sets used in proposed method test

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -38,31 +38,24 @@ def all_data(include=None, exclude=None):
 
 def data_for_proposed_method():
     return [
-        (DataSet('seismic'), 0.89),
-        (DataSet('yeast'), 0.59),
-        (DataSet('wpbc'), 0.8),
-        (DataSet('cylinder'), 0.66),
-        (DataSet('wilt'), 0.98),
-        (DataSet('ecoli'), 0.85),
-        (DataSet('flag'), 0.67),
-        (DataSet('spambase'), 0.9),
-        (DataSet('wdbc'), 0.90),
-        (DataSet('nursery'), 0.70),
-        (DataSet('car'), 0.75),
-        (DataSet('egg'), 0.4),
-        (DataSet('chess'), 0.90),
-        (DataSet('occupancy'), 0.90),
-        (DataSet('statlogSegment'), 0.8),
-        (DataSet('magic04'), 0.83),
+        (DataSet('balanceScale'), 0.82),
+        (DataSet('car'), 0.70),
+        (DataSet('chess'), 0.87),
+        (DataSet('cylinder'), 0.70),
+        (DataSet('ecoli'), 0.78),
+        (DataSet('glass'), 0.58),
         (DataSet('iris'), 0.95),
-        (DataSet('balanceScale'), 0.84),
-        (DataSet('banknote'), 0.96),
-        (DataSet('glass'), 0.60),
-        (DataSet('credit'), 0.85),
-        (DataSet('adult'), 0.83),
-        (DataSet('messidor'), 0.66),
-        (DataSet('mushroom'), 0.91),
+        (DataSet('magic04'), 0.83),
+        (DataSet('messidor'), 0.65),
+        (DataSet('mushroom'), 0.92),
+        (DataSet('nursery'), 0.65),
+        (DataSet('occupancy'), 0.90),
+        (DataSet('spambase'), 0.85),
+        (DataSet('statlogSegment'), 0.90),
+        (DataSet('wdbc'), 0.93),
+        (DataSet('wilt'), 0.95)
     ]
+
 
 def load_data_sets(names):
     return [DataSet(name) for name in names]
